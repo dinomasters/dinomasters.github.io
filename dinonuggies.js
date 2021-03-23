b1eb483aa58536a0e5d42935fc05f570fba5ec65
dinomasters <81229258+dinomasters@users.noreply.github.com>
2021-03-23 16:12:22 +0100

    y

--- dinonuggies.js
+++ dinonuggies.js
@@ -1,6 +1,12 @@
-document.onload = function()
+document.addEventListener('DOMContentLoaded', () => 
 {
-    let status = document.getElementById("status");
-    console.log(status.innerHTML)
-    status.innerHTML('uncooked');
-}
+    const cookButton = document.querySelector('#cook');
+    const status = document.querySelector('#status');
+
+    function turnonoven()
+    {
+        status.innerHTML = 'oven turned on!'
+    }
+
+    cookButton.addEventListener('click', turnonoven);
+});
