9810d0f2da0f05be1e36e62a04b29b0c9e0fbef8
dinomasters <81229258+dinomasters@users.noreply.github.com>
2021-03-23 11:35:08 +0100

    big changes

--- dinonuggies.js
+++ dinonuggies.js
@@ -1,13 +1,1 @@
-let dinonuggies = "bad";
-
-console.log('dino nuggies = ' + dinonuggies);
-
-function cook()
-{
-    dinonuggies = "good";
-    console.log('cooking...');
-}
-
-cook();
-
-console.log('dino nuggies = ' + dinonuggies);
+let status = document.getElementById('status');
