0912842d6892084e21842da50b64777ca6679d09
dinomasters <81229258+dinomasters@users.noreply.github.com>
2021-03-23 11:11:01 +0100

    epiic

---
+++ dinonuggies.js
@@ -0,0 +1,13 @@
+let dinonuggies = "bad";
+
+console.log('dino nuggies = ' + dinonuggies);
+
+function cook()
+{
+    dinonuggies = "good";
+    console.log('cooking...');
+}
+
+cook();
+
+console.log('dino nuggies = ' + dinonuggies);
